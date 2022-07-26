52a0925f3de66c36b21829fc52644760bde759d8
Sunairaa <sunairashahzeb@gmail.com>
2022-07-26 13:43:11 +0200

    fixed some bugs

--- js/index.js
+++ js/index.js
@@ -2,13 +2,11 @@
 
 function updateSubtotal(product) {
   let updateValue = 0;
-  console.log('Calculating subtotal, yey!');
   const price = product.querySelector('.price span').innerHTML;
   const quantity = product.querySelector('.quantity input').value;
-  updateSubTotalValue = Number(price) * Number(quantity);
+  updateValue = Number(price) * Number(quantity);
   let subtotal = product.querySelector('.subtotal span');
-  subtotal.innerHTML = `${Number(updateSubTotalValue)}`;
-  console.log(subtotal, subtotal.innerText)
+  subtotal.innerHTML = `${Number(updateValue)}`;
   return subtotal.innerHTML;
 }
 
@@ -26,7 +24,7 @@ function calculateAll() {
   for(let i = 0; i < products.length; i++) {
     subtotalArray.push(updateSubtotal(products[i]));
   }
-  console.log(subtotalArray)
+
   // ITERATION 3
   let total = document.querySelector('#total-value span');
   let calculateTotal = 0;
@@ -54,31 +52,35 @@ function createProduct() {
   let tablebody = document.querySelector('#cart tbody');
   const productRow = document.createElement('tr');
   productRow.setAttribute('class','product');
-  const productName = document.querySelector('.create-product td input[type = text]').value;
-  const price = document.querySelector('.create-product td input[type = number]').value;
+  const productName = document.querySelector('.create-product td input[type = text]');
+  const price = document.querySelector('.create-product td input[type = number]');
   productRow.innerHTML += `<td class="name">
-        <span>${productName}</span>
+        <span>${productName.value}</span>
         </td>
-        <td class="price">$<span>${Number(price).toFixed(2)}</span></td>
+        <td class="price">$<span>${Number(price.value).toFixed(2)}</span></td>
         <td class="quantity">
           <input type="number" value="0" min="0" placeholder="Quantity" />
         </td>
         <td class="subtotal">$<span>0</span></td>
         <td class="action">
-          <button class="btn btn-remove" onClick = ${removeProduct(e)}>Remove</button>
+          <button class="btn btn-remove">Remove</button>
         </td>
       </tr>
     `;
   tablebody.appendChild(productRow);
+  productName.value = "";
+  price.value = "0";
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
+
   const removeProductBtn = document.querySelectorAll('.btn-remove');
   for(let i = 0; i < removeProductBtn.length; i++) {
     removeProductBtn[i].addEventListener('click', removeProduct);
   }
+
   const createProductBtn = document.querySelector('#create');
   createProductBtn.addEventListener('click', createProduct);
 });
